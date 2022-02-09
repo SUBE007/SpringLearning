6f9a4990f764aae1f7df9d8701010e3325960e77
SUBE007 <chaurasiyasube007@gmail.com>
2022-02-09 19:22:21 +0530

    added First api as Get method

---
+++ src/test/java/com/dailycodebuffer/springboot/tutorialearning/SpringBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.springboot.tutorialearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
